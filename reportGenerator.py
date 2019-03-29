3b7df5a755326f939689fff464727c131411bc4e
Jeehay <wisdomjhkwon@gmail.com>
2019-03-29 17:39:30 +0900

    add pages

--- reportGenerator.py
+++ reportGenerator.py
@@ -14,8 +14,6 @@ import hashlib
 
 from datetime import datetime
 
-from sampleUpload import upload_sample
-
 def get_auth_file(fileaddr):
     try:
         with open(fileaddr, "r") as f:
@@ -53,6 +51,17 @@ def format_bytes(bytes_num):
 
     return str(round(dblbyte, 2)) + " " + sizes[i]
 
+def get_utc_local_time():
+    time_list = []
+
+    utc_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    local_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    time_list.append(utc_time)
+    time_list.append(local_time)
+
+    return time_list
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='ReversingLabs Korea - Report Generator Using A1000 api')
     parser.add_argument('--auth', metavar='AUTH', required=True, help='auth data file')
@@ -65,37 +74,34 @@ if __name__ == "__main__":
     hash_type = 'sha1'
     result_format = 'json' # can be xml
 
-    time_list = []
-
-    utc_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-    local_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    time_list.append(utc_time)
-    time_list.append(local_time)
+    time_list = get_utc_local_time()
 
     if args['upload'] != None and args['auth'] != None:
         authdata = get_auth_file(args['auth'])
         addr = authdata['addr']
         token = authdata['token']
         file_list = get_upload_file_list(args['upload'])
-        su = upload_sample(addr)
         index = 1
 
         data = {}
         data_categorized_number = {"known":0, "unknown":0, "suspicious":0, "malicious":0}
-
 
         for f in file_list:
             if f[0] is "~" :
                 hash_code = f[1:]
                 file_name = f[1:]
             else:
-                hash_code = su.upload(authdata['username'], authdata['password'], f, meta)
+                response = requests.post('%s/api/uploads/' % addr, files={'file': open(f, 'rb')},
+                            headers = {'Authorization': 'Token %s' % token})
+
+                response_json = json.loads(response.text)
+
+                hash_code = response_json["detail"]['sha1']
                 file_name = os.path.basename(f)
 
-            print("hash_code:", hash_code)
+            #print("hash_code:", hash_code)
 
-            ticoredata = requests.get(addr+'/api/samples/'+hash_code+'/ticore/',
+            ticoredata = requests.get('%s/api/samples/%s/ticore/' % (addr, hash_code),
                             headers = {'Authorization': 'Token %s' % token})
 
             ticore = json.loads(ticoredata.text)
@@ -105,40 +111,52 @@ if __name__ == "__main__":
                    "classification_origin", "classification_reason",\
                    "threat_status", "trust_factor", "threat_level", "threat_name",\
                    "summary", "ticloud", "aliases"]}
-
-            listdata = requests.post(addr+'/api/samples/list/',
+            print(post_data)
+            listdata = requests.post('%s/api/samples/list/' % addr,
                             data=post_data,
                             headers = {'Authorization': 'Token %s' % token})
 
-            result = json.loads(listdata.text)
-            result = result['results'][0]
+            if listdata.status_code != 200:
+                print('status : ', listdata.status_code)
+            else:
+                result = json.loads(listdata.text)
 
-            savefile_name = str(index)+'_'+file_name
+                if result['count'] is 0 :
+                    print('result not found- hash', hash_code)
+                else:
+                    result = result['results'][0]
 
-            data[hash_code] = [f, result['threat_status'], savefile_name]
-            print(result['threat_status'])
-            data_categorized_number[result['threat_status'].lower()]+=1
+                    savefile_name = str(index)+'_'+file_name
 
-            ##
-            file_loader = FileSystemLoader('./')
-            env = Environment(loader = file_loader)
-            tmpl_detail = env.get_template('template.html')
-            with open('result\\'+savefile_name+'.html', "w", encoding='utf-8') as fp :
-                fp.write(tmpl_detail.render(ticore = ticore, time_list = time_list, file_name = file_name, result = result))
-                print(os.getcwd()+"\\"+'result\\'+savefile_name+'.html SAVED')
+                    data[hash_code] = [f, result['threat_status'], savefile_name]
+                    data_categorized_number[result['threat_status'].lower()]+=1
 
-            index+=1
+                    filesize_formatted = format_bytes(result['file_size'])
+                    result['file_size'] = filesize_formatted
+                    ##
+                    file_loader = FileSystemLoader('./')
+                    env = Environment(loader = file_loader)
+                    tmpl_detail = env.get_template('summarypage_template.html')
+                    with open('result\\%s.html' % savefile_name , "w", encoding='utf-8') as fp :
+                        fp.write(tmpl_detail.render(ticore = ticore, time_list = time_list, savefile_name = savefile_name, result = result))
+                        print(os.getcwd()+"\\result\\%s.html SAVED" % savefile_name)
 
+                    tmpl_detail2 = env.get_template('info-file_template.html')
+                    with open('result\\%s+info_file.html' % savefile_name, "w", encoding='utf-8') as fp :
+                        fp.write(tmpl_detail2.render(ticore = ticore, time_list = time_list, savefile_name = savefile_name, result = result))
+                        print(os.getcwd()+"\\result\\%s+info_file.html SAVED" % savefile_name)
 
+                    index+=1
 
-    print("FINISH")
-
-    stf = open("summarypage_template.html", "r", encoding='utf-8')
+    stf = open("mainpage_template.html", "r", encoding='utf-8')
     summarytmpl = Template(stf.read())
 
     with open('result\\summarypage.html', "w", encoding='utf-8') as fp :
         fp.write(summarytmpl.render(data = data, time_list = time_list, data_categorized_number = data_categorized_number))
         print(os.getcwd()+"\\"+"result\\"+"summarypage.html"+" SAVED")
+
+    print("FINISH")
+
 
 
 
